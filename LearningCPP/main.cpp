af1bf00956448ce25f4a54e471caf5ba152d2901
MagmMe <marcin@magm.me>
2020-07-13 22:25:45 +0200

    calculating in CPP

--- LearningCPP/main.cpp
+++ LearningCPP/main.cpp
@@ -8,8 +8,22 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int uczniowie;
+int cukierki;
+int x;
+int y;
+
+int main() {
+    std::cout << "Ile uczniow jest w klasie?:\n";
+    std::cin >> uczniowie;
+    std::cout << "Ile masz cukierkow?:\n";
+    std::cin >> cukierki;
+    
+    x = cukierki - (uczniowie - 1);
+    std::cout << "Kazdy z uczniow dostanie:\n" << x;
+    
+    y = cukierki - x * (uczniowie - 1);
+    std::cout << "Tobie zostanie:\n" << y;
+    
     return 0;
 }
